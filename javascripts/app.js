6de9c5a65d459ab977fd0c2496f8cf6bdd9d751c
Jim Tryon <jtryon@mail.greenriver.edu>
2015-01-13 17:51:57 -0800

    Add tab content, button click handlers, event listener.  Remove hardcoded todo elements.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,33 +1,62 @@
-var main = function() {
-	"use strict";
+var main = function () {
+    var toDos = ["Get groceries",
+                 "Make up some new ToDos",
+                 "Prep for Monday's class",
+                 "Answer emails",
+                 "Take Gracie to the park",
+                 "Finish writing this book"];
 
-	/*var tabNumber;
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
 
-	for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
-		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+        // create a click handler for this element
+        $element.on("click", function () {
+            var $content,
+                $input,
+                $button,
+                i;
 
-		$(tabSelector).on("click", function() {
-			$(".tabs span").removeClass("active");
-			$(tabSelector).addClass("active");
-			return false;*/
-	$(".tabs a span").toArray().forEach(function (element) {
-		// create a click handler for this element
-		$(element).on("click", function () {
-			$(".tabs a span").removeClass("active");
-			$(element).addClass("active");
-			$("main .content").empty();
-			return false;
-		});
-	});
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+                // newest first, so we have to go through
+                // the array backwards
+                $content = $("<ul>");
+                for (i = toDos.length-1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+            } else if ($element.parent().is(":nth-child(2)")) {
+                // oldest first, so we go through the array forwards
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+            } else if ($element.parent().is(":nth-child(3)")) {
+                // input a new to-do
+                $input = $("<input>"),
+                $button = $("<button>").text("+");
+
+                $button.on("click", function () {
+                    if ($input.val() !== "") {
+                        toDos.push($input.val());
+                        $input.val("");
+                    }
+                });
+
+                $content = $("<div>").append($input).append($button);
+               /* Alternatively append() allows multiple arguments so the above
+                can be done with $content = $("<div>").append($input, $button); */
+            }
+
+            $("main .content").append($content);
+
+            return false;
+        });
+    });
+
+    $(".tabs a:first-child span").trigger("click");
 };
 
-/*var makeTabActive = function (tabNumber) {
-	// construct the selector from the tabNumber
-	var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-	$(".tabs span").removeClass("active");
-	$(tabSelector).addClass("active");
-};
-
-
-};
-*/
+$(document).ready(main);
