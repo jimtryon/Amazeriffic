ed61783be6061d7911d30066c5e8b9d9dff1c787
Jim Tryon <jtryon@mail.greenriver.edu>
2015-01-13 16:15:30 -0800

    Add loops for tab functionality

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,28 +1,33 @@
 var main = function() {
 	"use strict";
 
-	var makeTabActive = function (tabNumber) {
-		// construct the selector from the tabNumber
+	/*var tabNumber;
+
+	for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
 		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		$(".tabs span").removeClass("active");
-		$(tabSelector).addClass("active");
-	};
 
-	$(".tabs a:nth-child(1)").on("click", function () {
-		// make all the tabs inactive
-		makeTabActive(1);
-		return false;
-	});
-
-	$(".tabs a:nth-child(1)").on("click", function () {
-		// make all the tabs inactive
-		makeTabActive(2);
-		return false;
-	});
-
-	$(".tabs a:nth-child(1)").on("click", function () {
-		// make all the tabs inactive
-		makeTabActive(3);
-		return false;
+		$(tabSelector).on("click", function() {
+			$(".tabs span").removeClass("active");
+			$(tabSelector).addClass("active");
+			return false;*/
+	$(".tabs a span").toArray().forEach(function (element) {
+		// create a click handler for this element
+		$(element).on("click", function () {
+			$(".tabs a span").removeClass("active");
+			$(element).addClass("active");
+			$("main .content").empty();
+			return false;
+		});
 	});
 };
+
+/*var makeTabActive = function (tabNumber) {
+	// construct the selector from the tabNumber
+	var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+	$(".tabs span").removeClass("active");
+	$(tabSelector).addClass("active");
+};
+
+
+};
+*/
