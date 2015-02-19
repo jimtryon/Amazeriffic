2350aebea36c8ae5d48648ce47e5dbe2c0462bfd
Jim Tryon <jtryon@mail.greenriver.edu>
2015-02-19 02:18:03 -0800

    Added server code, updated post route

--- server.js
+++ server.js
@@ -1,21 +1,56 @@
 var express = require("express"),
     http = require("http"),
     app = express(),
-    toDos = {
-        // set up todo list here by copying
-        // the content from todos.OLD.json
-    };
+    toDos = [
+        {
+            "description" : "Get groceries",
+            "tags"  : [ "shopping", "chores" ]
+        },
+        {
+            "description" : "Make up some new ToDos",
+            "tags"  : [ "writing", "work" ]
+        },
+        {
+            "description" : "Prep for Monday's class",
+            "tags"  : [ "work", "teaching" ]
+        },
+        {
+            "description" : "Answer emails",
+            "tags"  : [ "work" ]
+        },
+        {
+            "description" : "Take Gracie to the park",
+            "tags"  : [ "chores", "pets" ]
+        },
+        {
+            "description" : "Finish writing this book",
+            "tags"  : [ "writing", "work" ]
+        }
+    ];
 
-    app.use(express.static(__dirname + "/client"));
+app.use(express.static(__dirname + "/client"));
 
-    http.createServer(app).listen(3000);
+// tell Express to parse incoming
+// JSON objects
+app.use(express.urlencoded());
 
-    // This route takes the place of our
-    // todos.json file in our example from
-    // Chapter 5
-    app.post("/todos", function (req, res) {
-        console.log("data has been posted to the server!");
+http.createServer(app).listen(3000);
 
-        // send back a simple object
-        res.json({"message": "You posted to the server!"});
-    });
+// This route takes the place of our
+// todos.json file in our example from
+// Chapter 5
+app.get("/todos.json", function (req, res) {
+    res.json(toDos);
+});
+
+app.post("/todos", function (req, res) {
+    // the object is now stored in req.body
+    var newToDo = req.body;
+
+    console.log(newToDo);
+
+    toDos.push(newToDo);
+
+    // send back a simple object
+    res.json({"message":"You posted to the server!"});
+});
